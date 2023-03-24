6b8859bd57d4e770c24eff7aca2c24ace4aef0c4
Carlos Eduardo <101908027+CarlosETS@users.noreply.github.com>
2023-03-24 20:18:19 -0300

    criação da entidade turma

---
+++ src/Domain/Models/Turma/TurmaModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Biopark.CpaSurvey.Domain.Models.Turma;
+
+public class TurmaModel { 
+    public string Nome { get; set; }
+
+    public DateTime Dt_inicio { get; set; }
+
+    public DateTime Dt_final { get; set; }
+
+    public long CursoId { get; set; }
+
+}
+
