74c5c4e2a51f6bf1b42b530fa2641460fc7eee7a
Carlos Eduardo <101908027+CarlosETS@users.noreply.github.com>
2023-03-24 21:57:47 -0300

    criação do command removerTurmaCommand

---
+++ src/Application/Turmas/Commands/RemoverTurma/RemoverTurmaCommand.cs
@@ -0,0 +1,36 @@
+﻿using Biopark.CpaSurvey.Domain.Entities.Turma;
+using Biopark.CpaSurvey.Domain.Interfaces.Infrastructure;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biopark.CpaSurvey.Application.Turmas.Commands.RemoverTurma;
+
+public class RemoverTurmaCommand : IRequest
+{
+    public long TurmaId { get; set; }
+}
+
+public class RemoverTurmaCommandHandler : IRequestHandler<RemoverTurmaCommand>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public RemoverTurmaCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Unit> Handler(RemoverTurmaCommand request, CancellationToken cancellationToken)
+    {
+        var repository = _unitOfWork.GetRepository<Turma>();
+
+        var turma = await repository
+            .FindBy(c => c.Id == request.TurmaId)
+            .FirstAsync(cancellationToken);
+
+        repository.Delete(turma);
+
+        await _unitOfWork.CommitAsync();
+
+        return Unit.Value;
+    }
+}
