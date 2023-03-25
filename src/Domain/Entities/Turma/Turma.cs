0facdb3458dae18be75318d6d1b5dad0d6788a94
Carlos Eduardo <101908027+CarlosETS@users.noreply.github.com>
2023-03-24 21:14:07 -0300

    criação do tumar configuration

--- src/Domain/Entities/Turma/Turma.cs
+++ src/Domain/Entities/Turma/Turma.cs
@@ -1,7 +1,27 @@
 ﻿using Biopark.CpaSurvey.Domain.Interfaces;
+using Biopark.CpaSurvey.Domain.Models.Turma;
 
 namespace Biopark.CpaSurvey.Domain.Entities.Turma;
 
 public class Turma : BaseEntity<long>, IAggregateRoot {
-    
+    public Turma(TurmaModel model) 
+    {
+        Nome  = model.Nome;
+        Dt_inicio = model.Dt_inicio;
+        Dt_finale = model.Dt_final;
+        CursoId = model.CursoId;
+    }
+
+    private Turma() { 
+    }
+
+    public string Nome { get; private set; }
+
+    public DateTime Dt_inicio { get; private set; }
+
+    public DateTime Dt_finale { get; private set; }
+
+    public long CursoId { get; private set; }
+
+    public Curso curso { get; private set; }
 }
--- src/Infra/Data/Configurations/TurmaConfiguration.cs
+++ src/Infra/Data/Configurations/TurmaConfiguration.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Biopark.CpaSurvey.Domain.Entities.Turma;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
-namespace Biopark.CpaSurvey.Infra.Data.Configurations
+
+namespace Biopark.CpaSurvey.Infra.Data.Configurations;
+
+public class TurmaConfiguration : IEntityTypeConfiguration<Turma>
 {
-    internal class TurmaConfiguration
+    public object Configure(EntityTypeBuilder<Turma> builder)
     {
+        builder.ToTable("turma");
+
+        builder.HasKey(e => e.Id);
+
+        builder.Property(p => p.Nome)
+            .HasColumnName("nome")
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(p => p.Dt_inicio)
+            .HasColumnName("dt_inicio")
+            .IsRequired();
+
+        builder.Property(p => p.Dt_finale)
+            .HasColumnName("dt_final")
+            .IsRequired();
+
+        builder.HasIndex(p => p.CursoId);
+
+        builder.Property(p => p.Curso)
+            .WithMany(e => e.Turma)
+            .HasForeignKey(p => p.CursoId)
+            .HasConstraintName("FK_Turma_Curso_CursoId");
     }
 }
