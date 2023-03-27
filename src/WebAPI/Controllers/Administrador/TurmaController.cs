ef758020e08a8e4a332f72ce405457e8e6ca5518
Carlos Eduardo <101908027+CarlosETS@users.noreply.github.com>
2023-03-27 19:00:27 -0300

    Criado turma controller

--- src/WebAPI/Controllers/Administrador/TurmaController.cs
+++ src/WebAPI/Controllers/Administrador/TurmaController.cs
@@ -1,6 +1,46 @@
-﻿namespace Biopark.CpaSurvey.WebAPI.Controllers.Administrador
+﻿
+using Biopark.CpaSurvey.Application.Turmas.Commands.CriarTurma;
+using Biopark.CpaSurvey.Application.Turmas.Queries.GetTurma;
+using Biopark.CpaSurvey.Infra.CrossCutting.Wrappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biopark.CpaSurvey.WebAPI.Controllers.Administrador;
+
+public class TurmaController : ApiController
 {
-    public class TurmaController
+    [HttpPost]
+
+    public async Task<IActionResult> PostAsync([FromBody] CriarTurmaCommand command)
     {
+        var result = await Mediator.Send(command);
+        return Created("turma/", new Response(result, "Turma cadastrada com sucesso"));
+
+    }
+
+    [HttpGet]
+
+    public async Task<IActionResult> GetAsync([FromQuery] GetTurmaQuery query)
+    {
+        var result = await Mediator.Send(query);
+
+        return Ok(result);
+    }
+
+    [HttpGet("{TurmaId:long}")]
+
+    public async Task<IActionResult> GetAsync([FromRoute] GetTurmaQuery query)
+    {
+        var result = await Mediator.Send(query);
+
+        return Ok(result);
+    }
+
+    [HttpDelete("{TurmaId:long}")]
+
+    public async Task<IActionResult> DeleteAsync([FromRoute] GetTurmaQuery query)
+    {
+        var result = await Mediator.Send(query);
+
+        return Ok(result, "Turma removida com sucesso");
     }
 }
