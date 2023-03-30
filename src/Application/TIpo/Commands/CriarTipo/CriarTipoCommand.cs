b06d39694a2464ef90bd8d7833203d118f75c772
Carlos Eduardo <101908027+CarlosETS@users.noreply.github.com>
2023-03-29 21:21:01 -0300

    Criado TipoCommands

---
+++ src/Application/TIpo/Commands/CriarTipo/CriarTipoCommand.cs
@@ -0,0 +1,45 @@
+﻿using Biopark.CpaSurvey.Domain.Entities.Tipo;
+using Biopark.CpaSurvey.Domain.Interfaces.Infrastructure;
+using Biopark.CpaSurvey.Domain.Models.Tipo;
+using MediatR;
+
+namespace Biopark.CpaSurvey.Application.TIpo.Commands.CriarTipo;
+public class CriarTipoCommand : IRequest<Tipo>
+{
+
+    public string Nome { get; set; }
+}
+
+public class CriarTipoCommandHandler : IRequestHandler<CriarTipoCommand, Tipo>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    public CriarTipoCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Tipo> Handle(CriarTipoCommand request, CancellationToken cancellationToken)
+    {
+        var model = CriarModelo(request);
+
+        var tipoInserir = new Tipo(model);
+
+        var repositoryTipo = _unitOfWork.GetRepository<Tipo>();
+
+        repositoryTipo.Add(tipoInserir);
+
+        await _unitOfWork.CommitAsync();
+
+        return tipoInserir;
+    }
+
+    public TipoModel CriarModelo(CriarTipoCommand request)
+    {
+        var model = new TipoModel
+        {
+            Nome = request.Nome,
+        };
+
+        return model;
+    }
+}
