28ff9bdda6bccd48282a6e3d2a3feaf951c57fae
Carlos Eduardo <101908027+CarlosETS@users.noreply.github.com>
2023-03-29 21:52:08 -0300

    Criado GetTiposQuery

---
+++ src/Application/TIpos/Queries/GetTipos/GetTumasQuery.cs
@@ -0,0 +1,31 @@
+﻿using Biopark.CpaSurvey.Domain.Entities.Tipo;
+using Biopark.CpaSurvey.Domain.Interfaces.Infrastructure;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biopark.CpaSurvey.Application.Tipos.Queries.GetTipos;
+
+public class GetTiposQuery : IRequest<List<Tipo>>
+{
+}
+
+public class GetTiposQueryHandler : IRequestHandler<GetTiposQuery, List<Tipo>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public GetTiposQueryHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<List<Tipo>> Handle(GetTiposQuery request, CancellationToken cancellationToken)
+    {
+        var repository = _unitOfWork.GetRepository<Tipo>();
+
+        var tipos = await repository
+            .GetAll()
+            .ToListAsync(cancellationToken);
+
+        return tipos;
+    }
+}
