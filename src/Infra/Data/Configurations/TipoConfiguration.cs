610eb38fddd2adcd61b5d1161a8eeb0004543bdd
Carlos Eduardo <101908027+CarlosETS@users.noreply.github.com>
2023-03-29 21:54:54 -0300

    Criado TipoConfiguration

---
+++ src/Infra/Data/Configurations/TipoConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Biopark.CpaSurvey.Domain.Entities.Tipo;
+
+namespace Biopark.CpaSurvey.Infra.Data.Configurations;
+public class TipoConfiguration : IEntityTypeConfiguration<Tipo>
+{
+    public void Configure(EntityTypeBuilder<Tipo> builder)
+    {
+        builder.ToTable("Tipo");
+
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.Nome)
+            .HasColumnName("Nome")
+            .IsRequired()
+            .HasMaxLength(50);
+    }
+}
