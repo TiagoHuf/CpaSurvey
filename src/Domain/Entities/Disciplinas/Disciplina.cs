347f4383ea8ef1410b7cabbeeb08a24ef7eb6e5f
Carlos Eduardo <101908027+CarlosETS@users.noreply.github.com>
2023-03-31 19:36:57 -0300

    Criado Entitie Disciplina

--- src/Domain/Entities/Disciplinas/Disciplina.cs
+++ src/Domain/Entities/Disciplinas/Disciplina.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Biopark.CpaSurvey.Domain.Models.Disciplinas;
 
-namespace Biopark.CpaSurvey.Domain.Entities.Disciplinas
+namespace Biopark.CpaSurvey.Domain.Entities.Disciplinas;
+
+public class Disciplina : BaseEntity<long>
 {
-    internal class Disciplina
-    {
+    public Disciplina(DisciplinasModel model) 
+    { 
+        Nome = model.Nome;
     }
+
+    private Disciplina() { }
+
+    public string Nome { get; private set; }
 }
