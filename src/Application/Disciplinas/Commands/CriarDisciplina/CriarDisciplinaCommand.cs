9ae33dff8cc2de7cddd563f150c6743e0ede0204
Carlos Eduardo <101908027+CarlosETS@users.noreply.github.com>
2023-03-31 19:41:19 -0300

    Criado CriarDisciplinasCommand

---
+++ src/Application/Disciplinas/Commands/CriarDisciplina/CriarDisciplinaCommand.cs
@@ -0,0 +1,47 @@
+﻿using Biopark.CpaSurvey.Domain.Entities.Disciplinas;
+using Biopark.CpaSurvey.Domain.Interfaces.Infrastructure;
+using Biopark.CpaSurvey.Domain.Models.Disciplinas;
+using MediatR;
+
+namespace Biopark.CpaSurvey.Application.Disciplinas.Commands.CriarDisciplina;
+public class CriarDisciplinaCommand : IRequest<Disciplina>
+{
+    public string Nome { get; set; }
+
+    public string Descricao { get; set; }
+}
+
+public class CriarDisciplinaCommandHandler : IRequestHandler<CriarDisciplinaCommand, Disciplina>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CriarDisciplinaCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Disciplina> Handle(CriarDisciplinaCommand request, CancellationToken cancellationToken)
+    {
+        var model = CriaModelo(request);
+
+        var DisciplinaInserir = new Disciplina(model);
+
+        var repositoryDisciplina = _unitOfWork.GetRepository<Disciplina>();
+
+        repositoryDisciplina.Add(DisciplinaInserir);
+
+        await _unitOfWork.CommitAsync();
+
+        return DisciplinaInserir;
+    }
+
+    public DisciplinasModel CriaModelo(CriarDisciplinaCommand request)
+    {
+        var model = new DisciplinasModel
+        {
+            Nome = request.Nome,
+        };
+
+        return model;
+    }
+}
