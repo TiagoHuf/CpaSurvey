c00207714ed36365be74c3ff5a2da9170be37472
Carlos Eduardo <101908027+CarlosETS@users.noreply.github.com>
2023-03-31 19:57:40 -0300

    Criado DisciplinasController

---
+++ src/WebAPI/Controllers/Administrador/DisciplinaController.cs
@@ -0,0 +1,35 @@
+﻿using Biopark.CpaSurvey.Application.Disciplinas.Queries.GetDisciplinas;
+using Biopark.CpaSurvey.Application.Disciplinas.Commands.CriarDisciplina;
+using Biopark.CpaSurvey.Application.Disciplinas.Queries.GetDisciplina;
+using Biopark.CpaSurvey.Infra.CrossCutting.Wrappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biopark.CpaSurvey.WebAPI.Controllers.Administrador;
+public class DisciplinasController : ApiController
+{
+    [HttpPost]
+    public async Task<IActionResult> PostAsync([FromBody] CriarDisciplinaCommand command)
+    {
+        var result = await Mediator.Send(command);
+        return Created(
+            "Disciplinas/",
+            new Response(result, "Disciplina cadastrado com sucesso.")
+        );
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAsync([FromQuery] GetDisciplinasQuery query)
+    {
+        var result = await Mediator.Send(query);
+
+        return Ok(result);
+    }
+
+    [HttpGet("{DisciplinaId:long}")]
+    public async Task<IActionResult> GetAsync([FromRoute] GetDisciplinaQuery query)
+    {
+        var result = await Mediator.Send(query);
+
+        return Ok(result);
+    }
+}
