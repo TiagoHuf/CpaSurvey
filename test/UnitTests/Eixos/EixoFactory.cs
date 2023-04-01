c4ea0f703bb6b8e26f36e8e0f785b9e2b0af81aa
TiagoHuf <tiago.hen.99@gmail.com>
2023-03-31 22:05:21 -0300

    Implementação parcial dos testes

---
+++ test/UnitTests/Eixos/EixoFactory.cs
@@ -0,0 +1,15 @@
+﻿using Biopark.CpaSurvey.Domain.Models.Eixos;
+
+namespace Biopark.CpaSurvey.UnitTests.Eixos;
+
+public class EixoFactory
+{
+    public static EixoModel GetEixoNovoModel()
+    {
+        return new EixoModel
+        {
+            Nome = "Nome Teste",
+            Descricao = "Descrição teste",
+        };
+    }
+}
---
+++ test/UnitTests/Turmas/TurmaFactory.cs
@@ -0,0 +1,21 @@
+﻿using Biopark.CpaSurvey.Domain.Entities.Turmas;
+using Biopark.CpaSurvey.Domain.Models.Turmas;
+
+namespace Biopark.CpaSurvey.UnitTests.Turmas;
+
+public class TurmaFactory
+{
+    public static TurmaModel GetTurmaNovaModel(string nome)
+    {
+        return new TurmaModel
+        {
+            Nome = nome,
+            CursoId = 1,
+        };
+    }
+
+    public static Turma GetTurmaNova(string nome)
+    {
+        return new Turma(GetTurmaNovaModel(nome));
+    }
+}
