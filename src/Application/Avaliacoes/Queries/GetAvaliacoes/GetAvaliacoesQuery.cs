9785662e818c31259ab2246fd13234f21c859a0b
Carlos Eduardo <101908027+CarlosETS@users.noreply.github.com>
2023-04-10 21:22:50 -0300

    Criado GetAvaliacoesQuery

---
+++ src/Application/Avaliacoes/Queries/GetAvaliacoes/GetAvaliacoesQuery.cs
@@ -0,0 +1,31 @@
+﻿using Biopark.CpaSurvey.Domain.Entities.Avaliacoes;
+using Biopark.CpaSurvey.Domain.Interfaces.Infrastructure;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biopark.CpaSurvey.Application.Avaliacoes.Queries.GetAvaliacoes;
+
+    public class GetAvaliacoesQuery : IRequest<List<Avaliacao>>
+{
+}
+
+public class GetAvaliacoesQueryHandler : IRequestHandler<GetAvaliacoesQuery, List<Avaliacao>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public GetAvaliacoesQueryHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<List<Avaliacao>> Handle(GetAvaliacoesQuery request, CancellationToken cancellationToken)
+    {
+        var repository = _unitOfWork.GetRepository<Avaliacao>();
+
+        var Avaliacaos = await repository
+            .GetAll()
+            .ToListAsync(cancellationToken);
+
+        return Avaliacaos;
+    }
+}
