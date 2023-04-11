24e32c1c990c6b0f10994f5a3fdbb1a9a881e3e3
Carlos Eduardo <101908027+CarlosETS@users.noreply.github.com>
2023-04-10 21:14:45 -0300

    Criado RemoverAvaliacaoCommand

---
+++ src/Application/Avaliacoes/Commands/RemoverAvaliacao/RemoverAvaliacaoCommand.cs
@@ -0,0 +1,36 @@
+﻿using Biopark.CpaSurvey.Domain.Entities.Avaliacoes;
+using Biopark.CpaSurvey.Domain.Interfaces.Infrastructure;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biopark.CpaSurvey.Application.Avaliacoes.Commands.RemoverAvaliacao;
+
+public class RemoverAvaliacaoCommand : IRequest
+{
+    public long AvaliacaoId { get; set; }
+}
+
+public class RemoverAvaliacaoCommandHandler : IRequestHandler<RemoverAvaliacaoCommand>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public RemoverAvaliacaoCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Unit> Handle(RemoverAvaliacaoCommand request, CancellationToken cancellationToken)
+    {
+        var repository = _unitOfWork.GetRepository<Avaliacao>();
+
+        var avaliacao = await repository
+            .FindBy(c => c.Id == request.AvaliacaoId)
+            .FirstAsync(cancellationToken);
+
+        repository.Delete(avaliacao);
+
+        await _unitOfWork.CommitAsync();
+
+        return Unit.Value;
+    }
+}
