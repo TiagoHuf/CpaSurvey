b00bc910f48639862ccfe2668273dc69bcb2cc6c
Carlos Eduardo <101908027+CarlosETS@users.noreply.github.com>
2023-04-10 21:12:02 -0300

    Criado CriarAvaliacaoCommand

---
+++ src/Application/Avaliacoes/Commands/CriarAvalicao/CriarAvaliacaoCommand.cs
@@ -0,0 +1,46 @@
+﻿using Biopark.CpaSurvey.Domain.Entities.Avaliacoes;
+using Biopark.CpaSurvey.Domain.Interfaces.Infrastructure;
+using Biopark.CpaSurvey.Domain.Models.Avaliacoes;
+using MediatR;
+
+namespace Biopark.CpaSurvey.Application.Avaliacoes.Commands.CriarAvalicao;
+
+public class CriarAvaliacaoCommand : IRequest<Avaliacao>
+{
+    public string Nome { get; set; }
+}
+
+public class CriarAvaliacaoCommandHandler : IRequestHandler<CriarAvaliacaoCommand, Avaliacao>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CriarAvaliacaoCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Avaliacao> Handle(CriarAvaliacaoCommand request, CancellationToken cancellationToken)
+    {
+        var model = CriaModelo(request);
+
+        var avaliacaoInserir = new Avaliacao(model);
+
+        var repositoryAvaliacoes = _unitOfWork.GetRepository<Avaliacao>();
+
+        repositoryAvaliacoes.Add(avaliacaoInserir);
+
+        await _unitOfWork.CommitAsync();
+
+        return avaliacaoInserir;
+    }
+
+    public AvaliacaoModel CriaModelo(CriarAvaliacaoCommand request)
+    {
+        var model = new AvaliacaoModel
+        {
+            Nome = request.Nome,
+        };
+
+        return model;
+    }
+}
