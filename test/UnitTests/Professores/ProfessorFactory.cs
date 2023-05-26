c4594c3d56e77a93ddb4659108987c0e69d1fe5a
CarlosETS <mrduingles@gmail.com>
2023-05-26 09:57:12 -0300

    Criado ProfessorFactory

---
+++ test/UnitTests/Professores/ProfessorFactory.cs
@@ -0,0 +1,15 @@
+﻿
+using Biopark.CpaSurvey.Domain.Models.Professores;
+
+namespace Biopark.CpaSurvey.UnitTests.Professores;
+
+public class ProfessorFactory
+{
+    public static ProfessorModel GetProfessorNovoModel()
+    {
+        return new ProfessorModel
+        {
+            Nome = "Professor Teste"
+        };
+    }
+}
