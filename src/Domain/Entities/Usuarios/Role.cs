49b31d71eae72e5abccf2eb4dbac00398142951b
Carlos Eduardo Teixeira de Souza <carlos.souza@alunos.bpkedu.com.br>
2023-06-16 19:41:47 -0300

    Criado Role

---
+++ src/Domain/Entities/Usuarios/Role.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biopark.CpaSurvey.Domain.Entities.Usuarios;
+
+public enum Role
+{
+    /// <summary>
+    /// Escala de Likert.
+    /// </summary>
+    Admin,
+
+    /// <summary>
+    /// Escala de Likert.
+    /// </summary>
+    Professor,
+
+    /// <summary>
+    /// Escala de Likert.
+    /// </summary>
+    Aluno
+}
