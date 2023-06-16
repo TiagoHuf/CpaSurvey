bf0af102e40ec873d96c414ad38cc0d1b79357d6
Carlos Eduardo Teixeira de Souza <carlos.souza@alunos.bpkedu.com.br>
2023-06-16 20:03:02 -0300

    Criado CorrigirSenhaUsuarioCommandValidator

---
+++ src/Application/Usuarios/Commands/CorrigirSenha/CorrigirSenhaUsuarioCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using Biopark.CpaSurvey.Application.Common.Validators;
+using Biopark.CpaSurvey.Application.Usuarios.Commands.CorrigirLogin;
+using Biopark.CpaSurvey.Domain.Entities.Usuarios;
+using Biopark.CpaSurvey.Domain.Interfaces.Infrastructure;
+using FluentValidation;
+
+namespace Biopark.CpaSurvey.Application.Usuarios.Commands.CorrigirSenha;
+
+public class CorrigirSenhaUsuarioCommandValidator : ValidatorBase<CorrigirSenhaUsuarioCommand>
+{
+    public CorrigirSenhaUsuarioCommandValidator(IUnitOfWork unitOfWork) : base(unitOfWork)
+    {
+        RuleFor(p => p.Senha)
+            .NotEmpty()
+            .MinimumLength(2)
+            .MaximumLength(200);
+
+        RuleFor(p => p.UsarioId)
+            .MustExists<CorrigirSenhaUsuarioCommand, Usuario>(unitOfWork);
+    }
+}
