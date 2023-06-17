1f34767d3261834caf3131458c58cb8b30ad3cbd
Carlos Eduardo Teixeira de Souza <carlos.souza@alunos.bpkedu.com.br>
2023-06-16 21:08:51 -0300

    Criado CriarUsuarioCommandValidator

---
+++ src/Application/Usuarios/Commands/CriarUsuario/CriarUsuarioCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using Biopark.CpaSurvey.Application.Common.Validators;
+using Biopark.CpaSurvey.Application.Eixos.Commands.CriarPergunta;
+using Biopark.CpaSurvey.Domain.Entities.Eixos;
+using Biopark.CpaSurvey.Domain.Interfaces.Infrastructure;
+using FluentValidation;
+
+namespace Biopark.CpaSurvey.Application.Usuarios.Commands.CriarUsuario;
+
+public class CriarUsuarioCommandValidator : ValidatorBase<CriarUsuarioCommand>
+{
+    public CriarUsuarioCommandValidator (IUnitOfWork unitOfWork) : base(unitOfWork)
+    {
+        RuleFor(p => p.Login)
+            .NotEmpty()
+            .MinimumLength(2)
+            .MaximumLength(200);
+
+        RuleFor(p => p.Senha)
+            .NotEmpty()
+            .MinimumLength(2)
+            .MaximumLength(30);
+
+        RuleFor(p => p.Role)
+            .IsInEnum();
+    }
+}
